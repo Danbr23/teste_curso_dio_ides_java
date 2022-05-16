8a4cef52c07db501d52ea9e21bf106d91a3b42c2
Danbr23 <daniel.pereirafr23@gmail.com>
2022-05-16 09:08:31 -0300

    First commit

---
+++ src/primeiro_Programa_Java/primeiroPrograma.java
@@ -0,0 +1,10 @@
+package primeiro_Programa_Java;
+
+public class primeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Word!");
+	}
+
+}
